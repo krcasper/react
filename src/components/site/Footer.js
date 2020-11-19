d82628e09fcfc26e2b3fe788697b38e428b0954a
Kelly Casper <kellycasper@Kellys-MacBook-Air.local>
2020-11-18 22:03:48 -0500

    new files

---
+++ src/components/site/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Row } from "reactstrap";
+
+const Footer = () => {
+    return (
+        <footer>
+            <Row>
+                <p>&copy; Eleven Fifty 2020</p>
+            </Row>
+        </footer>
+    );
+};
---
+++ src/components/site/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar className="header">
+                <NavbarBrand href="/">React Library</NavbarBrand>
+            </Navbar>
+        </header>
+    )
+}
